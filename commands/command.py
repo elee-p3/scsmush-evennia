7b3435d82d2993e75d57d2c31ddc3f28eb6e6549
Ugen <nyoogen@gmail.com>
2021-08-16 21:23:37 -0700

    editing OOC to add tag, attempt #2

--- commands/command.py
+++ commands/command.py
@@ -226,9 +226,9 @@ class CmdOOC(default_cmds.MuxCommand):
 
         # Call the at_after_say hook on the character
         # caller.at_say(speech, msg_self=True)
-        message = ("|y<OOC>|n {0} says, \"{1}\"").format(self.caller.name, message)
+        msg = ("|y<OOC>|n {0} says, \"{1}\"").format(self.caller.name, message)
         caller.location.msg_contents(
-            message, from_obj=caller, options={"is_pose": True}
+            msg, from_obj=caller, options={"is_pose": True}
         )
 
 # class CmdZog(default_cmds.MuxCommand):
