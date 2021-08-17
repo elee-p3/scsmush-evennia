8f7face4efb18b01c31a76a645319b7a1415e8ac
Ugen <nyoogen@gmail.com>
2021-08-16 20:19:29 -0700

    adding spoof command

--- commands/command.py
+++ commands/command.py
@@ -10,26 +10,37 @@ from evennia.commands.command import Command as BaseCommand
 # from evennia import default_cmds
 
 
-class Command(BaseCommand):
+class CmdSpoof(BaseCommand):
     """
-    Inherit from this if you want to create your own command styles
-    from scratch.  Note that Evennia's default commands inherits from
-    MuxCommand instead.
+    Actual posing functionality
 
-    Note that the class's `__doc__` string (this text) is
-    used by Evennia to create the automatic help entry for
-    the command, so make sure to document consistently here.
+    Usage:
+       spoof <message>
 
-    Each Command implements the following methods, called
-    in this order (only func() is actually required):
-        - at_pre_cmd(): If this returns anything truthy, execution is aborted.
-        - parse(): Should perform any extra parsing needed on self.args
-            and store the result on self.
-        - func(): Performs the actual work.
-        - at_post_cmd(): Extra actions, often things done after
-            every command, like prompts.
-
+    Allows non-emoting posing
     """
+    key = "spoof"
+    locks = "cmd:all()"
+
+    def func(self):
+        """Run the spoof command"""
+
+        caller = self.caller
+
+        if not self.args
+            caller.msg("Say what?")
+            return
+
+        speech = self.args
+
+        # If speech is empty, stop here
+        if not speech:
+            return
+
+        # Call the at_after_say hook on the character
+        caller.at_say(speech)
+
+
 
     pass
 
