903e9bac6f34cb6f8a9acb445ea24f23dac15622
Ugen <nyoogen@gmail.com>
2021-12-30 20:56:47 -0800

    turn unique_accounts back into a list and sort it

--- commands/command.py
+++ commands/command.py
@@ -575,7 +575,7 @@ class CmdWho(default_cmds.MuxCommand):
         total_sessions = sorted(total_sessions, key=lambda o: o.account.key)
         session_accounts = [session.account.key for session in total_sessions]
 
-        unique_accounts = set(session_accounts)
+        unique_accounts = list(set(session_accounts)).sort()
         pruned_sessions = []
 
         for acct in unique_accounts:
