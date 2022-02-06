36c84cd960b76fc16a43b676b07a9e25f7e3946e
Ugen <nyoogen@gmail.com>
2022-02-05 17:15:22 -0800

    testing pulling data from RPEvent table

--- world/events/views.py
+++ world/events/views.py
@@ -4,7 +4,7 @@ from world.events.models import RPEvent
 class MyFirstViewClass(ListView):
     model = RPEvent
     template_name = "events/test.html"
-    #
+
     # def get_context_data(self, **kwargs):
     #     context = super(MyFirstViewClass, self).get_context_data(**kwargs)
     #     return context
