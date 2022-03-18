351249655123d37ba7e972356412bed60fe5a40a
Ugen <nyoogen@gmail.com>
2022-03-17 22:13:08 -0700

    replace \n with <br>

--- web/template_overrides/templatetags/help_filters.py
+++ web/template_overrides/templatetags/help_filters.py
@@ -5,5 +5,5 @@ register = template.Library()
 @register.filter
 def convert_newlines(input):
     # converts evennia newlines and the automatically generated xhtml line breaks to \n
-    output = input.replace("|/", "\\n").replace("<br />", "\\n")
+    output = input.replace("|/", "<br>").replace("<br />", "<br>")
     return output
