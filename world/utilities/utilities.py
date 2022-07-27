a72d1011e1d6e4e9c82763b864fdba94c688110c
Ugen <nyoogen@gmail.com>
2022-07-26 22:17:26 -0700

    - added a proper character search so that we don't find rando objects in rooms too. THANKS EVENNIA

---
+++ world/utilities/utilities.py
@@ -0,0 +1,7 @@
+def location_character_search(location):
+    location_objects = location.contents
+    characters = []
+    for obj in location_objects:
+        if "Character" in obj.db_typeclass_path:
+            characters.append(obj)
+    return characters
