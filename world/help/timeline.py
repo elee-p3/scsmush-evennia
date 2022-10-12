b8adbab4cf2bd0a6eb8317ec8dd3bb506564f29a
dancerdevin <devinrfitzpatrick@gmail.com>
2022-10-11 22:29:15 -0500

    Updating timeline.py -- it *technically* works!

--- world/help/timeline.py
+++ world/help/timeline.py
@@ -4,25 +4,18 @@ import csv
 
 # This manually creates the "timeline" help entry by reading the timeline.csv file
 # and using evtable to produce an ASCII table readable on the site and in the client.
+# NOTE: This produces a bunch of excessive borders and illegible artifacts! Horray!!!!!
+# But maybe we can make something of it yet. Step by step.
 
 # First, read the csv file.
 with open('world/help/timeline.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     # The first row will become the headers in the evtable and define the number of columns.
     row1 = next(csvreader)
-    timeline_table = evtable.EvTable(row1[0], row1[1], row1[2], row1[3], row1[4],
-                                 border="cells")
+    timeline_table = evtable.EvTable(row1[0], row1[1], row1[2], row1[3], row1[4], border="cells")
     # Iterate through subsequent rows in the CSV file and add to the evtable
-    # Not sure how to skip the first row after using it for the headers above, though.
     for row in csvreader:
-        timeline_table.add_row(row)
-    # PROBLEM: This doesn't work. It's trying to split a list -- not sure how or why.
+        timeline_table.add_row(row[0], row[1], row[2], row[3], row[4])
 
 # Create the help entry with the timeline_table as the contents.
-# I *think* evtable produces a string, in which case, it should theoretically work fine?
-entry = create_help_entry("timeline",
-                timeline_table,
-                category="Theme", locks="view:all()")
-
-# Currently not sure how to *run* create_help_entry, though, and get it to make the help file.
-# Can just run it using @py/edit once I get it working!
+entry = create_help_entry("timeline", timeline_table.__str__(), category="theme", locks="view:all()")
