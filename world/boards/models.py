590487b318a2ee7133275dd7f20786cca82bd54b
Daniel Otero <ohboyotero@gmail.com>
2023-01-25 23:58:45 -0800

    Created basic Post model and the relationship with Board. Added to admin.

--- world/boards/models.py
+++ world/boards/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 class Board(models.Model):
     name = models.CharField(max_length=256)
-    
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField(null=False, blank=True)
+
+    board = models.ForeignKey(
+        Board,
+        blank=True,
+        null=True,
+        related_name="posts",
+        on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(null=False, auto_now=True)
