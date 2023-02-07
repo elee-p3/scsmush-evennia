a2a2e578ff32f9b4549ab64744c05924c5acb372
Daniel Otero <ohboyotero@gmail.com>
2023-02-07 08:16:57 -0800

    Figured out the naming issue that was preventing post count from showing up in boards.html, added a description field so that I could test that in the UI, and added comments to the Boards models.py file.

--- world/boards/models.py
+++ world/boards/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 
+# Board (aka "message board" aka "bulletin board" aka "bboard") is a model representing
+# a single board/topic within SCSMUSH's bulletin board system.
+#
+# Boards are flat (non-hierarchical) and all represent top-level topics within the system.
+#
+# Each board can have many Posts within it, and has a strict ownership relationship over its
+# posts.
 class Board(models.Model):
-    name = models.CharField(max_length=256)
+    # The name/title of the bulletin board shown to the user.
+    name = models.CharField(null=False, blank=True, max_length=256)
 
+    # The description of the board shown to the user. The intent is for this to
+    # be short (1-2 sentences), but it was encoded as a TextField to avoid having to
+    # bother with error handling on content length.
+    description = models.TextField(null=False, blank=True)
+
+
+# A Post is a single announcement, message, article, etc. that is posted to a parent
+# Board (above). Posts are strictly owned by their parent Board: they cannot be included
+# in multiple Boards.
 class Post(models.Model):
+    # Title of the post shown to the user.
     title = models.CharField(max_length=256)
+
+    # The body (contents) of the post represented in Evennia markdown #becauseEvennia.
+    # This content will be formatted prior to display to the user.
     body = models.TextField(null=False, blank=True)
 
+    # Parent Board to which this Post is posted.
     board = models.ForeignKey(
         Board,
         blank=True,
