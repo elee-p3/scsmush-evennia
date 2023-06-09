b714d707dceeb28c4bacd3b3318349b2455677f6
Daniel Otero <ohboyotero@gmail.com>
2023-06-09 02:06:33 -0700

    Rearranged the web view for posts to provide a view which makes it inherently clear which post was read when a page is loaded (to allow for future read tracking).

--- world/boards/templatetags/boards_tags.py
+++ world/boards/templatetags/boards_tags.py
@@ -7,10 +7,23 @@ register = template.Library()
 def render_board_summary(board):
     return { "board": board }
 
-@register.inclusion_tag("boards/post_summary.html")
-def render_post_summary(post):
-    return { "post": post }
+@register.inclusion_tag("boards/post_list.html")
+def render_post_list(sorted_posts, focused_post):
+    return { "sorted_posts": sorted_posts, "focused_post": focused_post }
+
+@register.inclusion_tag("boards/post_nav.html")
+def render_post_nav(post, previous, next):
+    return { "post": post, "previous": previous, "next": next}
+
+@register.inclusion_tag("boards/post_detail.html")
+def render_post_detail(post, body_paragraphs):
+    return { "post": post, "body_paragraphs": body_paragraphs }
 
 @register.simple_tag
-def get_board_detail_url(post):
-    return reverse('boards:detail', kwargs={'post_id': post.id})
+def get_board_detail_url(board, post=None):
+    return reverse('boards:detail_for_post',
+                   kwargs={'board_id': board.id, 'post_id': post.id if post else None})
+
+@register.simple_tag
+def get_focused_style(post_to_style, focused_post):
+    return "focused" if post_to_style.id == focused_post.id else ""
--- world/boards/views.py
+++ world/boards/views.py
@@ -1,5 +1,7 @@
+import re
 from django.shortcuts import get_object_or_404, render
-from world.boards.models import Board
+from django.http import HttpResponseNotFound
+from world.boards.models import Board, Post
 
 # List page for all boards.
 def boards(request):
@@ -7,12 +9,58 @@ def boards(request):
     context = { "all_boards": all_boards }
     return render(request, "boards/list.html", context)
 
-def detail(request, post_id):
-    board = get_object_or_404(Board, pk=post_id)
+# Shows a board detail page which doubles as a post detail page. Somewhat like a playlist
+# view in a media app, we show one focused post in detail while also showing the list of
+# all posts in the board in chronological order.
+def detail(request, board_id, post_id=None):
+    board = get_object_or_404(Board, pk=board_id)
     sorted_posts = board.posts.order_by('created_at')
+
+    if post_id != None:
+        post = get_object_or_404(Post, pk=post_id)
+
+        # Make sure this post actually belongs to the specified board.
+        if post.board.id != board.id:
+            return HttpResponseNotFound
+    else:
+        # If no post is explicitly specified, just default to the most recent post.
+        post = sorted_posts.last()
+
+    previous, next = _find_prev_and_next(post, sorted_posts)
+    
     context = {
         "board": board,
+        "post": post,
+        "paragraphs": _get_body_paragraphs(post),
         "sorted_posts": sorted_posts,
+        "previous": previous,
+        "next": next,
         "user": request.user
     }
     return render(request, "boards/detail.html", context)
+
+# Returns a list of strings, each representing a paragraph from the body of the given post.
+def _get_body_paragraphs(post):
+    return re.split("\s*\n\s*", post.body.strip())
+
+# Retrieves the previous and subsequent posts relative to the given focused_post from the
+# list of sorted_posts.
+def _find_prev_and_next(focused_post, sorted_posts):
+    previous = None
+    next = None
+    post_found = False
+    for post in sorted_posts:
+        if post.id == focused_post.id:
+            post_found = True
+            continue
+
+        if not post_found:
+            previous = post
+        else:
+            next = post
+            break
+
+    if not post_found:
+        raise ValueError("Invalid arguments: focused_post (id: %s) not found in sorted_posts." % focused_post.id)
+
+    return previous, next
