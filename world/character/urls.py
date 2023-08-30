b75477549a7021c400173596029ae3eadaa4a6a3
Daniel Otero <ohboyotero@gmail.com>
2023-08-30 01:05:40 -0700

    Fixed renamed url function.

--- world/character/urls.py
+++ world/character/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from evennia.web.website import views as website_views
 from django.conf.urls import url
 
 urlpatterns = [
     path('', views.character, name='list'),
-    url(
+    re_path(
         r"^characters/detail/(?P<slug>[\w\d\-]+)/(?P<pk>[0-9]+)/$",
         website_views.CharacterDetailView.as_view(),
         name="character-detail",
