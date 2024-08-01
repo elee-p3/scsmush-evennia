76c123353c12837e187c57c224875c60182bb6d5
ohboyotero <ohboyotero@gmail.com>
2024-08-01 13:00:47 -0700

    Update boards UI to facilitate read message tracking (#8)
    * Created the boards app, registered it with the project, created the Board model with a dummy name field, made migrations, made a stupid list view, added boards to site nav, and hooked up board model to admin.
    
    * Added the most beautiful CSS to bring the boards list page to life. Lifechanging.
    
    * Created basic Post model and the relationship with Board. Added to admin.
    
    * Figured out the naming issue that was preventing post count from showing up in boards.html, added a description field so that I could test that in the UI, and added comments to the Boards models.py file.
    
    * Updated board.html look and actual real description.
    
    * Fixed up the look of boards list page some more.
    
    * Added basic views, cross-linking, and url handling for posts within a board.
    
    * Added the post author relationship (a character can author a post so they can be in-world/in-character) and updated views to account for the change.
    
    * Added subscription relationship to board model.
    
    * Updated the post author relationship to reference Character and not ObjectDB.
    
    * DELORTED BAD MIGRATIONS AND MAED NEW ONESIES. Also fixed 'world.apps' app name and changed relation from objects.Character (which doesn't work) to objects.ObjectDB.
    
    * Rearranged the web view for posts to provide a view which makes it inherently clear which post was read when a page is loaded (to allow for future read tracking).
    
    * Added read tracking on post detail view by a particular user.
    
    * Switched read semantics for post reading to reference accounts (for IRL humans) instead of characters.
    
    * Fully swapped over to using accounts for both subscription and readership, fixed the migrations, etc.
    
    * Removed superfluous comment (now that we're no longer referencing ObjectDB and instead the much more specific AccountDB for the affected fields).

--- world/boards/models.py
+++ world/boards/models.py
@@ -17,10 +17,9 @@ class Board(models.Model):
     # bother with error handling on content length.
     description = models.TextField(null=False, blank=True)
 
-    # The set of characters who are subscribed to this board.
-    # TODO(daniel): add validation that this is only ever set with characters.
+    # The set of accounts who are subscribed to this board.
     subscribers = models.ManyToManyField(
-        "objects.ObjectDB",
+        "accounts.AccountDB",
         blank=True,
         related_name="subscribed_boards",
     )
@@ -29,6 +28,11 @@ class Board(models.Model):
 # A Post is a single announcement, message, article, etc. that is posted to a parent
 # Board (above). Posts are strictly owned by their parent Board: they cannot be included
 # in multiple Boards.
+#
+# Note that there is asymmetry between authoring and reading posts:
+#  - Posts are AUTHORED by characters, in-universe. This is for fun, creativity, and privacy.
+#  - Posts are READ by accounts, i.e., humans IRL. All that matters is whether or not a particular
+#    human being has seen a post, not which character they were puppetting at the time.
 class Post(models.Model):
     # Title of the post shown to the user.
     title = models.CharField(max_length=256)
@@ -46,6 +50,10 @@ class Post(models.Model):
         on_delete=models.CASCADE)
     
     # The character responsible for authoring this messageboard post.
+    #
+    # Characters are used to keep posts in-universe, and to protect the privacy of the humans
+    # behind those characters (who may not want to reveal their account information).
+    #
     # TODO(daniel): add validation so that only Characters can be set here.
     author = models.ForeignKey(
         "objects.ObjectDB",
@@ -54,26 +62,30 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
     )
 
-    # Representing post read (past tense "red" not present tense "reed") status
-    # via presence of a join relationship between a character and a post that they
-    # have read. Presence of `post` in `character.posts_read` indicates that
-    # `character` has read `post`. Conversely, `post.readers` contains all characters
-    # who have read `post`.
-    # TODO(daniel): add valiation so that only Characters can be set here.
+    # `readers` represents post-read (past tense "red" not present tense "reed") status via presence
+    # of a join relationship between an ACCOUNT (not a character) and a post that they have read.
+    # Presence of `post` in `account.posts_read` indicates that the IRL human associated with
+    # `account` has read `post`. Conversely, `post.readers` contains all accounts that have read
+    # `post`.
+    #
+    # As mentioned above, post READS are tracked at the account level (IRL human being), while writes
+    # are tracked as actions of a particular puppetted character for in-universe whimsy and privacy.
     readers = models.ManyToManyField(
-        "objects.ObjectDB",
+        "accounts.AccountDB",
         related_name="posts_read",
         blank=True)
 
     created_at = models.DateTimeField(null=False, auto_now_add=True)
     updated_at = models.DateTimeField(null=False, auto_now=True)
 
-    # Given an instance of Character, returns true if the given Character has
-    # read this post.
-    def was_read_by(self, character):
-        matching_characters = self.readers.filter(pk=character.id)
-        if matching_characters.len() > 1 or matching_characters.len() <0:
+    # Given an instance of Account, returns true if the given Account has read this post.
+    def was_read_by(self, account):
+        matching_accounts = self.readers.filter(pk=account.id)
+
+        # Per many-to-many.add() semantics, this should never happen.
+        if matching_accounts.len() > 1:
             logger = logging.getLogger("django.request")
-            logger.error("Post#was_read_by() found multiple instances of a read for one character")
-        return self.readers.filter(pk=character.id).len() == 1
+            logger.error("Post#was_read_by() found multiple instances of a read for one character: #{account.id}")
+
+        return self.readers.filter(pk=account.id).len() == 1
 
--- world/boards/templatetags/boards_tags.py
+++ world/boards/templatetags/boards_tags.py
@@ -7,10 +7,26 @@ register = template.Library()
 def render_board_summary(board):
     return { "board": board }
 
-@register.inclusion_tag("boards/post_summary.html")
-def render_post_summary(post):
-    return { "post": post }
+@register.inclusion_tag("boards/post_list.html")
+def render_post_list(sorted_posts, focused_post):
+    return { "sorted_posts": sorted_posts, "focused_post": focused_post }
+
+@register.inclusion_tag("boards/post_nav.html")
+def render_post_nav(post, previous, next):
+    return { "post": post, "previous": previous, "next": next}
+
+@register.inclusion_tag("boards/post_detail.html")
+def render_post_detail(post, body_paragraphs):
+    return { "post": post, "body_paragraphs": body_paragraphs }
 
 @register.simple_tag
-def get_board_detail_url(post):
-    return reverse('boards:detail', kwargs={'post_id': post.id})
+def get_board_detail_url(board, post=None):
+    if post == None:
+      return reverse('boards:detail', kwargs={'board_id': board.id})
+    
+    return reverse('boards:detail_for_post',
+                   kwargs={'board_id': board.id, 'post_id': post.id})
+
+@register.simple_tag
+def get_focused_style(post_to_style, focused_post):
+    return "focused" if post_to_style.id == focused_post.id else ""
